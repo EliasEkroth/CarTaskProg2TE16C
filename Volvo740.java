803bc6e6fca1a56e62abc1fcb19c1a85fbaca567
Pickaro <33488369+Pickaro@users.noreply.github.com>
2019-01-25 12:10:20 +0100

    Update Volvo740.java

--- Volvo740.java
+++ Volvo740.java
@@ -4,8 +4,8 @@ public class Volvo740 {
 
 	public final static double trimFactor = 1.25;
 
-	public double enginePower; // Engine power of the car
-	public double currentSpeed; // The current speed of the car
+	private double enginePower; // Engine power of the car
+	private double currentSpeed; // The current speed of the car
 	public Color color; // Color of the car
 	public String modelName; // The car model name
 
