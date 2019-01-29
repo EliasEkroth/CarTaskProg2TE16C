e166b1663173b0fd63e24dd3fa9392837eb9dcd4
Linus O.känd <33488299+LinusNordberg@users.noreply.github.com>
2019-01-29 15:20:08 +0100

    Update Volvo740.java

--- Volvo740.java
+++ Volvo740.java
@@ -1,58 +1,14 @@
 import javafx.scene.paint.Color;
 
-public class Volvo740 {
+public class volvo740 extends car{
 
-	private final static double trimFactor = 1.25;
 
-	private double enginePower; // Engine power of the car
-	private double currentSpeed; // The current speed of the car
-	public Color color; // Color of the car
-	public String modelName; // The car model name
-
-	public Volvo740() {
+	private volvo740() {
 		color = Color.RED;
 		enginePower = 100;
 		modelName = "Volvo740";
 		stopEngine();
+		trimFactor = 1.25;
 	}
 
-	private double getEnginePower() {
-		return enginePower;
-	}
-
-	public double getCurrentSpeed() {
-		return currentSpeed;
-	}
-
-	public Color getColor() {
-		return color;
-	}
-
-	public void startEngine() {
-		currentSpeed = 0.1;
-	}
-
-	public void stopEngine() {
-		currentSpeed = 0;
-	}
-
-	private double speedFactor() {
-		return enginePower * 0.01 * trimFactor;
-	}
-
-	private void incrementSpeed(double amount) {
-		currentSpeed = getCurrentSpeed() + speedFactor() * amount;
-	}
-
-	private void decrementSpeed(double amount) {
-		currentSpeed = getCurrentSpeed() - speedFactor() * amount;
-	}
-
-	public void gas(double amount) {
-		incrementSpeed(amount);
-	}
-
-	public void brake(double amount) {
-		decrementSpeed(amount);
-	}
 }
