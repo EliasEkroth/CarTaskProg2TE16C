ac734039d75ef29134f32940b0797a288de55b22
LinusNordberg <linusoss@gmail.com>
2019-01-29 16:06:05 +0100

    Update Movable.java
    69 <3

--- Movable.java
+++ Movable.java
@@ -1,8 +1,38 @@
 
-
 public interface Movable {
-	
-	void move();
-	void turnLeft();
-	void turnRight(); 
+
+public void move() {
+
+        double radians = Math.toRadians(this.getRotate() - 90);
+
+        this.setTranslateX(this.getTranslateX() + Math.cos(radians) * 200);
+        this.setTranslateY(this.getTranslateY() + Math.sin(radians) * 200);
+
+        if(this.getTranslateY() <= -100){
+            this.setTranslateY(1000);
+        }
+
+        else if(this.getTranslateY() >= 1000){
+            this.setTranslateY(-100);
+        }
+
+        else if(this.getTranslateX() <= -100){
+            this.setTranslateX(1920);
+        }
+
+        else if(this.getTranslateX() >= 1920){
+            this.setTranslateX(-100);
+        }
+
+
+    }
+
+    public void turnLeft() {
+        this.setRotate(this.getRotate() - 20);
+    }
+
+    public void turnRight() {
+        this.setRotate(this.getRotate() + 20);
+    }
+    
 }
