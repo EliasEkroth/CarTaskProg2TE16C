cae139683268701b885100b62f44cfcc658273c2
LinusNordberg <linusoss@gmail.com>
2019-02-05 16:22:40 +0100

    Update Volvo740.java

--- src/Volvo740.java
+++ src/Volvo740.java
@@ -4,7 +4,7 @@ public class volvo740 extends car{
 
 
 	private volvo740() {
-		color = Color.RED;
+		color = Color.rgb(251, 255, 100); // T-GUL
 		enginePower = 130; //1982 års modell dåva  
 		modelName = "Volvo740";
 		stopEngine();
