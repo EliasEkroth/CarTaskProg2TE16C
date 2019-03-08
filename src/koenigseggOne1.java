2aa04d084206081110a1c6a3f30579f038af86b4
LinusNordberg <linusoss@gmail.com>
2019-03-08 12:20:28 +0100

    eyo

--- src/koenigseggOne1.java
+++ src/koenigseggOne1.java
@@ -6,7 +6,7 @@ public class koenigseggOne:1 extends car{
 
 	private koenigseggOne:1() {		//sätter den specifika datan för ferrari
 		color = Color.RED;
-		enginePower = 570 /*bulkade hästar*/;
+		enginePower = 1340 /*bulkade hästar*/;
 		modelName = "koenigseggOne:1";
 		stopEngine();
 	}
